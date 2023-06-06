087baba9c4655ea1361fa767ad3280557cb4e4a6
GRB.github <76528190+GRBgithub@users.noreply.github.com>
2023-06-06 14:07:14 +0200

    env

--- src/Utils/useSocket.js
+++ src/Utils/useSocket.js
@@ -16,7 +16,7 @@
 // }
 import io from 'socket.io-client';
 
-const socket = io('ws://localhost:8080');
+const socket = io(process.env.SOCKETURL);
 
 function useSocket() {
   React.useEffect(() => {
